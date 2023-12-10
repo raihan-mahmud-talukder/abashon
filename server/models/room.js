96f077bcfb769617669facf325d47f547a2f0d1d
Raihan Mahmud <raihan_uits@outlook.com>
2023-12-11 01:18:51 +0600

    users model

--- server/models/room.js
+++ server/models/room.js
@@ -1,22 +1,17 @@
 const mongoose = require('mongoose')
 
-const roomSchema = mongoose.Schema(
-    {
-        name: { type: String, required: true },
-        category: { type: Number, required: true },
-        capacity: { type: Number, required: true },
-        size: { type: Number, required: true },
-        price: { type: Number, required: true },
-        img: { type: String, required: true },
-        type: { type: String, required: true },
-        ac: { type: Boolean, required: true },
-        dining: { type: Boolean, required: true },
-        washroom: { type: Boolean, required: true },
-    },
-    {
-        timestamps: true
-    }
-)
+const roomSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    category: { type: Number, required: true },
+    capacity: { type: Number, required: true },
+    size: { type: Number, required: true },
+    price: { type: Number, required: true },
+    img: { type: String, required: true },
+    type: { type: String, required: true },
+    ac: { type: Boolean, required: true },
+    dining: { type: Boolean, required: true },
+    washroom: { type: Boolean, required: true },
+}, { timestamps: true })
 
 module.exports = mongoose.model('rooms', roomSchema)
 
