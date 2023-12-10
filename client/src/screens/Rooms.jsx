5eede8d54b4627f13cd05b33d735f49f81660b4c
Raihan Mahmud <raihan_uits@outlook.com>
2023-12-11 03:47:43 +0600

    rendering rooms

--- client/src/screens/Rooms.jsx
+++ client/src/screens/Rooms.jsx
@@ -1,5 +1,44 @@
+import { useEffect, useState } from "react"
+import axios from 'axios'
+
 export const Rooms = () => {
-  return (
-    <div>Rooms</div>
-  )
+    const [rooms, setRooms] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const data = (await axios.get('/api/rooms/getallrooms')).data
+                setRooms(data)
+            } catch (error) { console.log(error) }
+        }
+        fetchData()
+    }, [])
+
+    return (
+        <div className="rooms">
+            {rooms.map(room => { return <Room room={room} key={room._id} /> })}
+        </div>
+    )
+}
+
+const Room = ({ room }) => {
+    console.log(room)
+    return (
+        <div className="room">
+            <h3>{room.name}</h3>
+            <div className="info">
+                <img src={room.img} alt={room.name} />
+                <div className="details">
+                    <span><b>Room Category: </b>{room.category}</span><br />
+                    <span><b>Room Area: </b>{room.size} sqft</span><br />
+                    <span><b>Capacity: </b>{room.capacity} Person</span><br />
+                    <span><b>Air Conditioned: </b>{room.ac ? 'Yes' : 'No'}</span><br />
+                    <span><b>Washroom: </b>{room.washroom ? 'Attached' : 'Shared'}</span><br />
+                    <span><b>Dinning: </b>{room.dinning ? 'Seperate' : 'Shared'}</span><br />
+                    <p><b>Type:</b> {room.type}</p>
+                    <p><b>Price:</b> BDT {room.price}</p>
+                </div>
+            </div>
+        </div>
+    )
 }
