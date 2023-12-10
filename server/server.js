02d8d1e3b71b60546dfeafde3b613d3b8aac083d
Raihan Mahmud <raihan_uits@outlook.com>
2023-12-11 01:44:22 +0600

    server api

--- server/server.js
+++ server/server.js
@@ -12,6 +12,12 @@ app.use(express.json())
 
 app.use(cors())
 
+const roomsRoute = require('./routes/room')
+const usersRoute = require('./routes/user')
+
+app.use('/api/rooms', roomsRoute)
+app.use('/api/users', usersRoute)
+
 const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`Server running on port ${port} 🔥`))
