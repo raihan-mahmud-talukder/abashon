bd1845cb9db9b8ad47b3769e9e98bffeaa74bae7
Raihan Mahmud <raihan_uits@outlook.com>
2023-12-12 00:09:28 +0600

    bugfixed

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,19 @@
-import { NavLink, Route, Routes } from "react-router-dom"
+import { NavLink, Route, Routes, useNavigate } from "react-router-dom"
 import { Home } from "../screens/Home"
 import { About } from "../screens/About"
 import { Rooms } from "../screens/Rooms"
 import { Login } from "../screens/Login"
 import { Register } from "../screens/Register"
+import { useEffect, useState } from "react"
 
 export const Header = () => {
-    const user = JSON.parse(localStorage.getItem('currentUser'))
-    const logout = () => { localStorage.removeItem('currentUser') }
+    const [user, setUser] = useState(null)
+    const navigate = useNavigate() // needed for dynamic navbar
+    useEffect(() => {
+        const user = JSON.parse(localStorage.getItem('currentUser'))
+        setUser(user)
+    })
+    const logout = () => localStorage.removeItem('currentUser')
 
     return (
         <>
@@ -18,12 +24,15 @@ export const Header = () => {
                         <li><NavLink to='/' className={({ isActive }) => (isActive ? "active" : "")}>Home</NavLink></li>
                         <li><NavLink to='/about' className={({ isActive }) => (isActive ? "active" : "")}>About</NavLink></li>
                         <li><NavLink to='/rooms' className={({ isActive }) => (isActive ? "active" : "")}>Rooms</NavLink></li>
-                        {!user ? (
-                            <>
-                                <li><NavLink to='/login' className={({ isActive }) => (isActive ? "active" : "")}>Login</NavLink></li>
-                                <li><NavLink to='/register' className={({ isActive }) => (isActive ? "active" : "")}>Register</NavLink></li>
-                            </>
-                        ) : <li><NavLink to='/login' onClick={logout} >Logout</NavLink></li>
+                        {!user ?
+                            (
+                                <>
+                                    <li><NavLink to='/login' className={({ isActive }) => (isActive ? "active" : "")}>Login</NavLink></li>
+                                    <li><NavLink to='/register' className={({ isActive }) => (isActive ? "active" : "")}>Register</NavLink></li>
+                                </>
+                            )
+                            :
+                            <li><NavLink to='/login' onClick={logout} >Logout</NavLink></li>
                         }
                     </ul>
                 </menu>
