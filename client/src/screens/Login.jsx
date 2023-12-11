a7ead9092154f05e77a824a66ce012f5ba0b4900
Raihan Mahmud <raihan_uits@outlook.com>
2023-12-11 21:12:38 +0600

    login screen

--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -1,5 +1,43 @@
+import axios from 'axios'
+import { useState } from 'react'
+
 export const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const login = async () => {
+    const user = { email, password }
+    try {
+      const result = (await axios.post('/api/users/login', user)).data
+      localStorage.setItem('currentUser', JSON.stringify(result))
+      window.location.href = '/room'
+    } catch (error) { console.log(error) }
+  }
+
   return (
-    <div>Login</div>
+    <form onSubmit={login} className='login'>
+      <h2>LOGIN</h2>
+      <label htmlFor='email'>Email:</label>
+      <input
+        type="email"
+        placeholder="email"
+        required={true}
+        id='email'
+        value={email}
+        onChange={event => setEmail(event.target.value)}
+      /><br />
+      <label htmlFor="password">Password: </label>
+      <input
+        type="password"
+        placeholder="password"
+        required={true}
+        id="password"
+        value={password}
+        onChange={event => setPassword(event.target.value)}
+      /><br />
+      <button type="reset" value='reset'>RESET</button>
+      <button type="submit" value='submit'>LOGIN</button>
+      <span>Haven't registered? <a href='/register'>REGISTER</a></span>
+    </form>
   )
 }
